a641110088a7f827c87960aa92e980066cab72f6
risa-s-92 <r-sato-1d8@eagle.sophia.ac.jp>
2021-07-25 19:34:40 +0900

    js修正しました

--- main.js
+++ main.js
@@ -1,39 +1,39 @@
 //galleryのスライドショー
 const galleryScreen = document.getElementById("gallery_slide");
 const galleryArrowRight = document.getElementById("gallery-slide-arrow-right");
-const galleryArrowLeft = document.getElementById("gallery-slide-arrow-left")
+const galleryArrowLeft = document.getElementById("gallery-slide-arrow-left");
 
 //写真の情報
 let slideInfo = [
-    {image: "IMG_3473_day.jpg"}
-    {image: "IMG_3500_night.jpg"}
+  { image: "IMG_3473_day.jpg" },
+  { image: "IMG_3500_night.jpg" },
 ];
 
-let i= 0;
-galleryScreen.src = `./images/${slideInfo[0].image}`
+let i = 0;
+galleryScreen.src = `./images/${slideInfo[0].image}`;
 
 //次の写真に変える関数
 const changeImage = function () {
-    if (i === Number(slideInfo.length) -1) {
-        i = 0;
-    } else {
-        i++;
-    }
-    galleryScreen.src = `./images/${slideInfo[i].image}`
+  if (i === Number(slideInfo.length) - 1) {
+    i = 0;
+  } else {
+    i++;
+  }
+  galleryScreen.src = `./images/${slideInfo[i].image}`;
 };
 
 //right
-galleryArrowRight.onclick = function() {
-    changeImage();
+galleryArrowRight.onclick = function () {
+  changeImage();
 };
 
 //left
 galleryArrowLeft.onclick = function () {
-    if( i === 0) {
-        i = Number(slideinfo.length) -1;
-    } else {
-        i -= 1;
-    }
+  if (i === 0) {
+    i = Number(slideinfo.length) - 1;
+  } else {
+    i -= 1;
+  }
 
-galleryScreen.src = `./images/${slideinfo[i].image}`;
+  galleryScreen.src = `./images/${slideinfo[i].image}`;
 };
